d2c9f1292fc430dde31b3b69cc1ccb879521d207
Kevin Simpson <relasine@gmail.com>
2018-12-03 15:45:15 -0700

    Add endpoint skeletons

--- server.js
+++ server.js
@@ -12,6 +12,48 @@ app.use(express.static("public"));
 
 app.set("port", process.env.PORT || 3000);
 
+// -- DRIVERS -- //
+
+app.get("/api/v1/drivers", (request, response) => {});
+
+app.get("api/v1/drivers/:driver_name", (request, response) => {});
+
+app.patch("api/v1/drivers/:driver_id/points", (request, response) => {});
+
+app.patch("api/v1/drivers/:driver_id/standing", (request, response) => {});
+
+app.patch("api/v1/drivers/:driver_id/team", (request, response) => {});
+
+app.post("api/v1/drivers", (request, response) => {});
+
+app.delete("api/v1/drivers/:driver_id", (request, response) => {});
+
+// -- TEAMS -- //
+
+app.get("api/v1/teams", (request, response) => {});
+
+app.patch("api/v1/teams/:team_id/standing", (request, response) => {});
+
+app.patch("api/v1/teams/:team_id/podium_finishes", (request, response) => {});
+
+app.patch("api/v1/teams/:team_id/titles", (request, response) => {});
+
+app.post("api/v1/teams", (request, response) => {});
+
+app.delete("api/v1/teams/:team_id", (request, response) => {});
+
+// -- RACES -- //
+
+app.get("api/v1/races", (request, response) => {});
+
+app.get("api/v1/races?continent=:continent_name", (request, response) => {});
+
+app.post("api/v1/races", (request, response) => {});
+
+app.delete("api/v1/race/:race_id", (request, response) => {});
+
+// -- SETUP -- //
+
 app.listen(app.get("port"), () => {
   console.log(`${app.locals.title} is running on ${app.get("port")}`);
 });
