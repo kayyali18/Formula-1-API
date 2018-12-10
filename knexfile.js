e677d3096224d909d69fdd35d4518e80934c5d5b
Ahmad Kayyali <kayyali18@gmail.com>
2018-12-09 17:07:51 -0700

    Hoisting issue fix in knex_file

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,6 @@
+const DB_URL =
+  "postgres://bmvcctmtaagqql:7c3d48c94540219001bfb36f61ad151461e83aebd0f974a6e181a5b1d6272f40@ec2-54-197-234-33.compute-1.amazonaws.com:5432/d41hc10gn05mog";
+
 module.exports = {
   test: {
     client: "pg",
@@ -33,6 +36,3 @@ module.exports = {
     useNullAsDefault: true
   }
 };
-
-const DB_URL =
-  "postgres://bmvcctmtaagqql:7c3d48c94540219001bfb36f61ad151461e83aebd0f974a6e181a5b1d6272f40@ec2-54-197-234-33.compute-1.amazonaws.com:5432/d41hc10gn05mog";
