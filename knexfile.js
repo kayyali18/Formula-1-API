61ce7dfd906c1e49891a0b3606cf154babd2573b
Ahmad Kayyali <kayyali18@gmail.com>
2018-12-09 17:05:05 -0700

    Heroku and PostgreSQL setup for production

--- knexfile.js
+++ knexfile.js
@@ -21,5 +21,18 @@ module.exports = {
       directory: "./db/seeds/dev"
     },
     useNullAsDefault: true
+  },
+  production: {
+    client: "pg",
+    debug: true,
+    connection: DB_URL,
+    migrations: {
+      tableName: "./db/migrations"
+    },
+    ssl: true,
+    useNullAsDefault: true
   }
 };
+
+const DB_URL =
+  "postgres://bmvcctmtaagqql:7c3d48c94540219001bfb36f61ad151461e83aebd0f974a6e181a5b1d6272f40@ec2-54-197-234-33.compute-1.amazonaws.com:5432/d41hc10gn05mog";
